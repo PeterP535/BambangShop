d7f75976cdfafda2afc4bf7bd4e5fcc197efbcc1
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2025-03-28 18:38:33 +0700

    Implement add, list_all, and delete function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -10,4 +10,40 @@ lazy_static! {
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
+
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber.clone();
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+    
+        SUBSCRIBERS.get(product_type).unwrap()
+            .insert(subscriber_value.url.clone(), subscriber_value);
+        return subscriber;
+    }
+    
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+    
+        return SUBSCRIBERS.get(product_type).unwrap().iter()
+            .map(|f| f.value().clone()).collect();
+    }
+
+    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+    
+        let result = SUBSCRIBERS.get(product_type).unwrap()
+            .remove(url);
+    
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
+    }
+    
+    
 }
